4258c77c5f71c95ab34c311f3b9ceb8608493620
Bruno Ravenščak <bravens@algebra.hr>
2025-05-31 20:08:56 +0200

    Initial commit

---
+++ src/test/java/hr/java/web/prosport/ProSportApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.java.web.prosport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
